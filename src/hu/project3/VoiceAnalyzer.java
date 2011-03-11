ecdaf1e80e6b2ca655a1228d22fa6ca407aa3a07
Werring <thomwerring@gmail.com>
2011-03-11 13:22:21 +0100

    Display text on screen

--- src/hu/project3/VoiceAnalyzer.java
+++ src/hu/project3/VoiceAnalyzer.java
@@ -2,12 +2,16 @@ package hu.project3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class VoiceAnalyzer extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Een tekstje");
+        setContentView(tv);
+//      setContentView(R.layout.main);
     }
 }
