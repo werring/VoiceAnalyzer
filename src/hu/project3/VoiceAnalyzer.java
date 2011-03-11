bcba8d1a3edb1b1564811b56362aa2347fc52b5c
Werring <thomwerring@gmail.com>
2011-03-11 12:47:11 +0100

    New Eclipse project

---
+++ src/hu/project3/VoiceAnalyzer.java
@@ -0,0 +1,13 @@
+package hu.project3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VoiceAnalyzer extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
